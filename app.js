fa22385bedecdbd720d0d962b15ceaf89e10a437
debajit13 <mallickdebajit3@gmail.com>
2023-12-31 11:45:22 +0530

    add register and login routes and middleware to authenticate apis #3

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 require('./config/database').connect;
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const YAML = require('yaml');
 const swaggerUI = require('swagger-ui-express');
 const fs = require('fs');
@@ -9,6 +11,8 @@ const cors = require('cors');
 const file = fs.readFileSync('./swagger.yaml', 'utf8');
 const swaggerDocument = YAML.parse(file);
 const Todo = require('./models/todo');
+const User = require('./models/user');
+const auth = require('./middleware/auth');
 
 app.use(express.json());
 app.use(cors());
@@ -23,8 +27,111 @@ app.get('/api/v1/', (req, res) => {
   });
 });
 
+// route to register
+app.post('/api/v1/register', async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    if (!(firstName && lastName && email && password)) {
+      return res.status(400).json({
+        error: true,
+        message: 'firstName, lastName, email and password are required fields!',
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        error: true,
+        message: 'User is already existed!',
+      });
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    if (user) {
+      const token = jwt.sign(
+        {
+          user_id: user._id,
+          email,
+        },
+        process.env.SECRET,
+        {
+          expiresIn: '2h',
+        }
+      );
+      user.password = undefined;
+      user.token = token;
+    }
+
+    return res.status(200).json({
+      error: false,
+      message: 'User created successfully!',
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// route to login
+app.post('/api/v1/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      return res.status(400).json({
+        error: true,
+        message: 'email and password are required fields!',
+      });
+    }
+
+    const user = await User.findOne({
+      email: email.toLowerCase(),
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        error: true,
+        message: "User hasn't registered yet!",
+      });
+    }
+
+    if (user && bcrypt.compare(password, user.password)) {
+      const token = jwt.sign(
+        {
+          user_id: user._id,
+          email,
+        },
+        process.env.SECRET,
+        {
+          expiresIn: '2h',
+        }
+      );
+      user.password = undefined;
+      user.token = token;
+    }
+
+    return res.status(200).json({
+      error: false,
+      message: 'User loggedIn successfully!',
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 // route to get all the todos
-app.get('/api/v1/todos', async (req, res) => {
+app.get('/api/v1/todos', auth, async (req, res) => {
   const todos = await Todo.find({});
 
   return res.status(200).json({
@@ -35,7 +142,7 @@ app.get('/api/v1/todos', async (req, res) => {
 });
 
 // route to add new todo
-app.post('/api/v1/todos', async (req, res) => {
+app.post('/api/v1/todos', auth, async (req, res) => {
   try {
     const { title, description } = req.body;
 
@@ -62,7 +169,7 @@ app.post('/api/v1/todos', async (req, res) => {
 });
 
 // route to delete a todo by Id
-app.delete('/api/v1/todos', async (req, res) => {
+app.delete('/api/v1/todos', auth, async (req, res) => {
   const _id = req.query.id;
 
   if (!_id) {
@@ -91,7 +198,7 @@ app.delete('/api/v1/todos', async (req, res) => {
 });
 
 // route to update a todo by Id
-app.put('/api/v1/todos', async (req, res) => {
+app.put('/api/v1/todos', auth, async (req, res) => {
   const _id = req.query.id;
   const { title, description, isDone } = req.body;
 
